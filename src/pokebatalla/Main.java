c2513ffa18b9c6d17c0cbce0895a12ede1fa4ffa
JesusAlfonsochuchete <s22120196@alumnos.itsur.edu.mx>
2023-10-18 14:54:07 -0600

    Organización de carpetas y clases

--- src/pokebatalla/Main.java
+++ src/pokebatalla/Main.java
@@ -3,44 +3,97 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package pokebatalla;
+      //@JESUS ALFONSO MARTINEZ MARTINEZ
+//En este programa puse la mayoria de las cosas no supe muy bien somo poner los ataques 
+import pokebatalla.pokemones.Mew;
+import pokebatalla.pokemones.Vaporeon;
+import pokebatalla.pokemones.Dragonair;
+import pokebatalla.pokemones.Pokemon;
+import pokebatalla.pokemones.Gengar;
+import pokebatalla.pokemones.Charizard;
+import pokebatalla.pokemones.Gastly;
 
-/**
- *
- * @author FJML1983
- */
+
+
 public class Main {
-
+       
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello PokeBatalla!");
+
+        Mew M1 = new Mew(); //Instanciando               
+        Dragonair Dr = new Dragonair();   //Instanciando                     
+        Vaporeon Vp = new Vaporeon();//Instanciando
+        Gastly G1 = new Gastly();// Instanciando
+        Charizard Ch1 = new Charizard();//Instanciando
+        Gengar G2 = new Gengar();//Instanciando
+
+        System.out.println("----------------------------");
+        Pokemon obj = new Pokemon();
+        System.out.println("Ejile un pokemon");
+        obj.elejirPokemon("Vaporeon");
+        System.out.println("Elije un oponente");
+        obj.oponente("Gengar");
+        System.out.println("Pelea");
+        System.out.println(Vp + " VS " + G2);
+        System.out.println("Vaporio atacaka a :" + G2);
+        G2.recibirAtake(55);
+        G2.estatusBatalla();
+        System.out.println("Gengar contra ataque");
+        System.out.println(Vp);
+        M1.recibirAtake(50);
+        M1.estatusBatalla();
+        System.out.println("Cambio a Mew por " + Ch1);
+        System.out.println("Charizard ataka a " + G2);
+        G2.resAtakeEspescial(70);
+        G2.estatusBatalla();
+        G2.estaVivo();
+        System.out.println("---------------------------");
         
-        Pikachu p1 = new Pikachu();        
-        p1.tipo = "ELECTRICO";
-        p1.hp = 35;
-        p1.ataque = 55;
-        p1.defensa = 30;
-        p1.nivel = 1;
-        p1.precision = 4;
+
+        System.out.println(M1);
+        System.out.println(Dr);
+        System.out.println(Vp);
+        System.out.println(G1);
+        System.out.println(Ch1);
+        System.out.println(G2);
+
+        System.out.println("El estatus actual de Mew es:");
+        M1.status();
+
+        System.out.println("El estatus actual de Dragonair es:");
+        Dr.status();
+
+        System.out.println("El estatus actual de Vaporeon es:");
+        Vp.status();
+
+        System.out.println("El estatus actual de Gastly es:");
+        G1.status();
+
+        System.out.println("El estatus actual de Charizard es:");
+        Ch1.status();
+        System.out.println("El estatus acutal de Gengear es:");
+        G2.status();
         
-        Charmander cha1 = new Charmander();        
-        cha1.tipo = "FUEGO";
-        cha1.hp = 39;
-        cha1.ataque = 52;
-        cha1.defensa = 43;
-        cha1.nivel = 1;
-        cha1.precision = 4;
         
-        Bullbasaur b1 = new Bullbasaur();
-        b1.tipo = "PLANTA/VENENO";
-        b1.hp = 45;
-        b1.ataque = 49;
-        b1.defensa = 49;
-        b1.nivel = 1;
-        b1.precision = 4;
+        M1.ataqueespecial= "Hipnosis";
+        Dr.ataqueespecial= "Danza dragon";
+        Vp.ataqueespecial= "Water Absorb";
+        G1.ataqueespecial= "Onda toxica";
+        Ch1.ataqueespecial= "Sismico";
+        G2.ataqueespecial= "Onda tóxica";
+        
+        System.out.println("Mew tiene un ataque especial que seria: " +M1.ataqueespecial);
+        System.out.println("Dragonite tiene un ataque especial que seria: " +Dr.ataqueespecial);
+        System.out.println("Vaporio tiene un ataque especial que seria: " +Vp.ataqueespecial);
+        System.out.println("Gastly tiene un ataque especial que seria: " +G1.ataqueespecial);
+        System.out.println("Charizar tiene un ataque especial que seria: " +Ch1.ataqueespecial);
+        System.out.println("Gengar tiene un ataque especial que seria: " +G2.ataqueespecial);
+
+        
+        
         
     }
-    
+
 }
---
+++ src/pokebatalla/pokemones/Gengar.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pokebatalla.pokemones;
+
+import pokebatalla.*;
+import java.util.List;
+
+/**
+ *
+ *///@JESUS ALFONSO MARTINEZ MARTINEZ
+ //
+public class Gengar extends Pokemon {
+
+    String ataqueespecial;
+    Gengar(){
+        this.tipo= "FANTASMA/VENENO";
+        this.hp = 60;
+        this.ataque = 65;
+        this.defensa = 60;
+        this.nivel = 1;
+        this.precision = 7;
+    }
+public Gengar(String nombre){
+    this();
+    this.nombre = nombre;
+}    
+
+    public Gengar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    public double getPrecision() {
+        return precision;
+    }
+
+    public void setPrecision(double precision) {
+        this.precision = precision;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public List<String> getHabilidades() {
+        return habilidades;
+    }
+
+    public void setHabilidades(List<String> habilidades) {
+        this.habilidades = habilidades;
+    }
+
+
+}
